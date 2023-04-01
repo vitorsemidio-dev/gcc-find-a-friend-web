623155cbfb4b9e65bb49a704d6d0d9697b2287a6
Vitor Emídio <vitorsemidio.96@gmail.com>
2023-03-31 22:48:08 -0300

    feat: registro de organização

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,25 +1,52 @@
+import { FormEvent, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import LogoHorizontal from '@/assets/icons/logo-horizontal.svg'
+import Eye from '@/assets/icons/password-eye.svg'
+import Pets from '@/assets/icons/pets.svg'
+import { api } from '@/services/http'
+
 import {
-  Wrapper,
-  Container,
-  Card,
-  FormWrapper,
-  Form,
-  InputWrapper,
-  Buttons,
   Button,
+  Buttons,
+  Card,
+  Container,
+  Form,
+  FormWrapper,
+  InputWrapper,
+  Wrapper,
 } from './styles'
 
-import LogoHorizontal from '../../assets/icons/logo-horizontal.svg'
-import Pets from '../../assets/icons/pets.svg'
-import Eye from '../../assets/icons/password-eye.svg'
-
 export function Register() {
-  function handleRegisterOrganization() {
-    // TO dO
+  const [registerForm, setRegisterForm] = useState({
+    name: '',
+    email: '',
+    address: '',
+    cep: '',
+    whatsappNumber: '',
+    password: '',
+    passwordConfirm: '',
+  })
+
+  const navigate = useNavigate()
+
+  async function handleRegisterOrganization(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    try {
+      const response = await api.post('/orgs', registerForm)
+      console.log(response)
+      handleLoginOrganization()
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   function handleRenderMapLocation() {
-    // TO DO
+    // TODO
+  }
+
+  function handleLoginOrganization() {
+    navigate('/login')
   }
 
   return (
@@ -31,7 +58,21 @@ export function Register() {
         </Card>
         <FormWrapper>
           <h1>Cadastre sua organização</h1>
-          <Form>
+          <Form onSubmit={(e) => handleRegisterOrganization(e)}>
+            <label htmlFor="name">Nome</label>
+            <InputWrapper>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="John Doe"
+                value={registerForm.name}
+                onChange={(e) =>
+                  setRegisterForm({ ...registerForm, name: e.target.value })
+                }
+              />
+            </InputWrapper>
+
             <label htmlFor="email">Email</label>
             <InputWrapper>
               <input
@@ -39,6 +80,10 @@ export function Register() {
                 name="email"
                 id="email"
                 placeholder="mayk@email.com"
+                value={registerForm.email}
+                onChange={(e) =>
+                  setRegisterForm({ ...registerForm, email: e.target.value })
+                }
               />
             </InputWrapper>
 
@@ -49,16 +94,27 @@ export function Register() {
                 name="address"
                 id="address"
                 placeholder="Rua do Meio, 1825"
+                value={registerForm.address}
+                onChange={(e) =>
+                  setRegisterForm({ ...registerForm, address: e.target.value })
+                }
               />
             </InputWrapper>
 
-            <label htmlFor="contact">Whatsapp</label>
+            <label htmlFor="whatsappNumber">Whatsapp</label>
             <InputWrapper>
               <input
                 type="text"
-                name="contact"
-                id="contact"
+                name="whatsappNumber"
+                id="whatsappNumber"
                 placeholder="99 99999 9999"
+                value={registerForm.whatsappNumber}
+                onChange={(e) =>
+                  setRegisterForm({
+                    ...registerForm,
+                    whatsappNumber: e.target.value,
+                  })
+                }
               />
             </InputWrapper>
 
@@ -69,23 +125,34 @@ export function Register() {
                 name="password"
                 id="password"
                 placeholder="Senha"
+                value={registerForm.password}
+                onChange={(e) =>
+                  setRegisterForm({ ...registerForm, password: e.target.value })
+                }
               />
               <img onClick={() => {}} src={Eye} alt="" />
             </InputWrapper>
 
-            <label htmlFor="confirmPassword">Confirmar senha</label>
+            <label htmlFor="passwordConfirm">Confirmar senha</label>
             <InputWrapper>
               <input
                 type="password"
-                name="confirmPassword"
-                id="confirmPassword"
+                name="passwordConfirm"
+                id="passwordConfirm"
                 placeholder="Confirme sua senha"
+                value={registerForm.passwordConfirm}
+                onChange={(e) =>
+                  setRegisterForm({
+                    ...registerForm,
+                    passwordConfirm: e.target.value,
+                  })
+                }
               />
               <img onClick={() => {}} src={Eye} alt="" />
             </InputWrapper>
 
             <Buttons>
-              <Button type="submit" onClick={() => {}} className="primary">
+              <Button type="submit" className="primary">
                 Cadastrar
               </Button>
             </Buttons>
